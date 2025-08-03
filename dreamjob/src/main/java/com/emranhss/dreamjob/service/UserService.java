ff83ca2012f97c23557b8dd77646927cf94a04ff
Sathe-Khanom <sathekhanom550@gmail.com>
2025-08-03 18:47:21 +0600

    add

--- dreamjob/src/main/java/com/emranhss/dreamjob/service/UserService.java
+++ dreamjob/src/main/java/com/emranhss/dreamjob/service/UserService.java
@@ -1,12 +1,23 @@
 package com.emranhss.dreamjob.service;
 
+import com.emranhss.dreamjob.dto.AuthenticationResponse;
 import com.emranhss.dreamjob.entity.JobSeeker;
 import com.emranhss.dreamjob.entity.Role;
+import com.emranhss.dreamjob.entity.Token;
 import com.emranhss.dreamjob.entity.User;
+import com.emranhss.dreamjob.jwt.JwtService;
+import com.emranhss.dreamjob.repository.ITokenRepository;
 import com.emranhss.dreamjob.repository.IUserRepo;
 import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -14,14 +25,20 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
     private IUserRepo userRepo;
+
+    @Autowired
+    private ITokenRepository tokenRepository;
 
     @Autowired
     private EmailService emailService;
@@ -29,9 +46,33 @@ public class UserService {
     @Autowired
     private JobSeekerService jobSeekerService;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    @Lazy
+    private AuthenticationManager authenticationManager;
+
 
     @Value("src/main/resources/static/images")
     private String uploadDir;
+
+    @Autowired
+
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
 
 
     public void saveOrUpdate(User user, MultipartFile imageFile) {
@@ -46,20 +87,25 @@ public class UserService {
     }
 
     public List<User> findAll() {
+
         return userRepo.findAll();
     }
 
     public User findById(int id) {
+
         return userRepo.findById(id).get();
     }
 
     public void delete(User user) {
+
         userRepo.delete(user);
     }
 
 
     private void sendActivationEmail(User user) {
         String subject = "Welcome to Our Service – Confirm Your Registration";
+
+        String activationLink="http://localhost:8085/api/user/active/"+user.getId();
 
         String mailText = "<!DOCTYPE html>"
                 + "<html>"
@@ -84,6 +130,8 @@ public class UserService {
                 + "      <p>If you have any questions or need help, feel free to reach out to our support team.</p>"
                 + "      <br>"
                 + "      <p>Best regards,<br>The Support Team</p>"
+                + "      <p>To Activate Your Account, please click the following link:</p>"
+                + "      <p><a href=\"" + activationLink + "\">Activate Account</a></p>"
                 + "    </div>"
                 + "    <div class='footer'>"
                 + "      &copy; " + java.time.Year.now() + " YourCompany. All rights reserved."
@@ -106,7 +154,7 @@ public class UserService {
         Path uploadPath = Paths.get(uploadDir + "/users");
         if (!Files.exists(uploadPath)) {
             try {
-                Files.createDirectories(uploadPath);
+                Files.createDirectory(uploadPath);
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -139,10 +187,10 @@ public class UserService {
             }
         }
 
-        String jobSeekerName = jobSeeker.getName() ;
-        String fileName = jobSeekerName.trim().replaceAll("\\s+", "_") ;
+        String jobSeekerName = jobSeeker.getName();
+        String fileName = jobSeekerName.trim().replaceAll("\\s+", "_");
 
-        String savedFileName = fileName+ "_" + UUID.randomUUID().toString();
+        String savedFileName = fileName + "_" + UUID.randomUUID().toString();
 
         try {
             Path filePath = uploadPath.resolve(savedFileName);
@@ -157,21 +205,101 @@ public class UserService {
 
     public void registerJobSeeker(User user, MultipartFile imageFile, JobSeeker jobSeekerData) {
         if (imageFile != null && !imageFile.isEmpty()) {
+            // Save image for both User and JobSeeker
             String filename = saveImage(imageFile, user);
             String jobSeekerPhoto = saveImageForJobSeeker(imageFile, jobSeekerData);
             jobSeekerData.setPhoto(jobSeekerPhoto);
             user.setPhoto(filename);
         }
 
+        // Encode password before saving User
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(Role.JOBSEEKER);
-        User savedUser = userRepo.save(user); // Save User first
 
-        // Set user to jobSeeker and save it
+        // Save User FIRST and get persisted instance
+        User savedUser = userRepo.save(user);
+
+        // Now, associate saved User with JobSeeker and save JobSeeker
         jobSeekerData.setUser(savedUser);
-
         jobSeekerService.save(jobSeekerData);
 
+        // Now generate token and save Token associated with savedUser
+        String jwt = jwtService.generateToken(savedUser);
+        saveUserToken(jwt, savedUser);
+
+        // Send Activation Email
         sendActivationEmail(savedUser);
     }
+
+
+
+    private void saveUserToken(String jwt, User user) {
+        Token token = new Token();
+        token.setToken(jwt);
+        token.setLogout(false);
+        token.setUser(user);
+
+        tokenRepository.save(token);
+
+    }
+
+    private void removeAllTokenByUser(User user) {
+
+        List<Token> validTokens = tokenRepository.findAllTokenByUser(user.getId());
+
+        if (validTokens.isEmpty()) {
+            return;
+        }
+        validTokens.forEach(t -> {
+            t.setLogout(true);
+        });
+
+        tokenRepository.saveAll(validTokens);
+
+    }
+
+
+    // It is Login Method
+    public AuthenticationResponse authencate(User request){
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        User user=userRepo.findByEmail(request.getEmail()).orElseThrow();
+
+        // Generate Token for Current User
+        String jwt=jwtService.generateToken(user);
+
+        //Remove all existing toke for this user
+        removeAllTokenByUser(user);
+
+        saveUserToken(jwt, user);
+
+        return  new AuthenticationResponse(jwt, "User Login Successful");
+
+    }
+
+
+    public  String activeUser(int id){
+
+        User user=userRepo.findById(id)
+                .orElseThrow(()-> new RuntimeException("User not Found with this ID "+id));
+
+        if(user !=null){
+            user.setActive(true);
+
+            userRepo.save(user);
+            return "User Activated Successfully!";
+
+        }else {
+            return  "Invalid Activation Token!";
+        }
+
+    }
+
 
 }
