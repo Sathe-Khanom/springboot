f9a0a5ed8585297cb597d0c23d404974d816eb3f
Sathe-Khanom <sathekhanom550@gmail.com>
2025-08-09 19:06:51 +0600

    add

--- dreamjob/src/main/java/com/emranhss/dreamjob/service/EducationService.java
+++ dreamjob/src/main/java/com/emranhss/dreamjob/service/EducationService.java
@@ -1,12 +1,15 @@
 package com.emranhss.dreamjob.service;
 
 import com.emranhss.dreamjob.entity.Education;
+import com.emranhss.dreamjob.entity.JobSeeker;
 import com.emranhss.dreamjob.repository.EducationRepository;
 import com.emranhss.dreamjob.repository.JobSeekerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class EducationService {
@@ -18,15 +21,24 @@ public class EducationService {
     private JobSeekerRepository jobSeekerRepository;
 
     public List<Education> getByJobSeekerId(Long jobSeekerId) {
-        return educationRepository.findByJobSeekerId(jobSeekerId);
+        List<Education> educations = educationRepository.findByJobSeekerId(jobSeekerId);
+        return educations.stream()
+                .map(Education :: new)
+                .collect(Collectors.toList());
     }
 
-    public Education save(Education education) {
+    public Education saveEducation(Education education, String email) {
+        JobSeeker jobSeeker = jobSeekerRepository.findByUserEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("JobSeeker not found"));
+
+        education.setJobSeeker(jobSeeker);
+
         return educationRepository.save(education);
     }
 
     public void delete(Long id) {
         educationRepository.deleteById(id);
     }
+
 
 }
